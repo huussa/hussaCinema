dcb78dc67e81089864b2433ed4ca0e097ddb621e
Hussam Alghamdi <hussam@example.com>
2026-08-11 22:05:02 +0300

    set up server.js closes #1

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
